ca82122378e4fd7022dd7ec4a92dd9733a8f896c
nretamar <nicolasretamar93@gmail.com>
2021-07-05 09:01:42 -0300

    Actualizacion texto TerminosyCondiciones

--- app/src/main/java/com/trotos/appsubastas/TerminosyCondiciones.java
+++ app/src/main/java/com/trotos/appsubastas/TerminosyCondiciones.java
@@ -4,16 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class TerminosyCondiciones extends AppCompatActivity {
+
+    TextView tyc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terminosy_condiciones);
 
+        tyc = findViewById(R.id.tyc);
+
+        tyc.setMovementMethod(new ScrollingMovementMethod());
 
     }
 }
